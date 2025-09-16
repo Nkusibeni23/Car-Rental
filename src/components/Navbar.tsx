abbd21f67d91a4b980b7989020f5ee25aae42a3d
Ndizihiwe Benny Chrispin <ndizibaidu23@gmail.com>
2025-09-16 16:24:03 +0200

    Adds lucide-react dependency and updates Navbar component
    Introduces the  library to enhance icon usage.
    
    Modifies the Navbar component by replacing the user icon with an image and adding a cart feature with a quantity badge.
    
    Improves the overall user interface and icon representation in the application.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,10 @@
 
 import { Fragment } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Menu, Transition } from "@headlessui/react";
-import { FaUser, FaShoppingCart, FaBars } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 import { HiChevronDown } from "react-icons/hi";
 import { useAppSelector, useAppDispatch } from "@/store/hooks";
 import { logout } from "@/store/authSlice";
@@ -36,21 +37,52 @@ export default function Navbar() {
             </Link>
           </div>
 
-          {/* Right side  */}
           <div className="flex items-center space-x-4">
-            {/* Cart Icon */}
-            <button className="text-gray-900 hover:text-gray-700 p-2">
-              <FaShoppingCart className="h-5 w-5" />
-            </button>
+            <div className="flex items-center space-x-3">
+              <div className="relative">
+                <button className="text-gray-900 hover:text-gray-700 p-2 relative cursor-pointer">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={35}
+                    height={35}
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      fillRule="evenodd"
+                      d="M8.418 3.25c.28-.59.884-1 1.582-1h4c.698 0 1.301.41 1.582 1c.683.006 1.216.037 1.692.223a3.25 3.25 0 0 1 1.426 1.09c.367.494.54 1.127.776 1.998l.742 2.722l.28.841l.024.03c.901 1.154.472 2.87-.386 6.301c-.546 2.183-.818 3.274-1.632 3.91c-.814.635-1.939.635-4.189.635h-4.63c-2.25 0-3.375 0-4.189-.635c-.814-.636-1.087-1.727-1.632-3.91c-.858-3.431-1.287-5.147-.386-6.301l.024-.03l.28-.841l.742-2.722c.237-.871.41-1.505.776-1.999a3.25 3.25 0 0 1 1.426-1.089c.476-.186 1.008-.217 1.692-.222m.002 1.502c-.662.007-.928.032-1.148.118a1.75 1.75 0 0 0-.768.587c-.176.237-.28.568-.57 1.635l-.57 2.089C6.384 9 7.778 9 9.684 9h4.631c1.907 0 3.3 0 4.32.18l-.569-2.089c-.29-1.067-.394-1.398-.57-1.635a1.75 1.75 0 0 0-.768-.587c-.22-.086-.486-.111-1.148-.118A1.75 1.75 0 0 1 14 5.75h-4a1.75 1.75 0 0 1-1.58-.998"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  {/* Quantity Badge */}
+                  <span className="absolute -top-1 -right-1 bg-black text-white text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center">
+                    3
+                  </span>
+                </button>
+              </div>
+
+              {/* Total Price */}
+              <div className="text-sm text-gray-700">
+                <span className="font-semibold">RWF 245,000</span>
+              </div>
+            </div>
+
+            <div className="h-8 w-px bg-gray-300"></div>
 
             {/* Profile Dropdown */}
             <Menu as="div" className="relative inline-block text-left">
               <div>
                 <Menu.Button
-                  className="flex items-center text-gray-900 hover:text-gray-700 p-2 rounded-md"
+                  className="flex items-center text-gray-900 hover:text-gray-700 p-2 rounded-md cursor-pointer"
                   onClick={!isAuthenticated ? handleProfileClick : undefined}
                 >
-                  <FaUser className="h-5 w-5" />
+                  <Image
+                    src="/images/abstract-user-flat-4.png"
+                    alt="User Icon"
+                    width={40}
+                    height={40}
+                    className="h-10 w-10"
+                  />
                   {isAuthenticated && (
                     <HiChevronDown className="ml-1 h-4 w-4" />
                   )}
