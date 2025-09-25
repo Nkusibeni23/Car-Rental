565951297c0207d673abd03f3eee9aa9c5d6615d
kwizera-bonheur123 <kbonheur123@gmail.com>
2025-09-25 22:07:32 +0200

    socket connection set up

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import socketReducer from "./slices/socketSlice";
+import { socketMiddleware } from "./middleware/socketMiddleware";
 
 export const store = configureStore({
   reducer: {
+    socket: socketReducer,
     auth: authReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore socket instance in serialization checks
+        ignoredActions: ["socket/setSocket"],
+        ignoredPaths: ["socket.socket"],
+      },
+    }).concat(socketMiddleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
